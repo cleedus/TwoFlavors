9f7c518c3a7116b7dc247f10f3dd816d838a7aa3
cleedus <cletus.mukolu@gmail.com>
2018-11-29 04:56:18 -0600

    fixed functional test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -1,28 +1,27 @@
 package com.udacity.gradle.builditbigger;
-
-import android.app.Instrumentation;
-import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.example.jokedisplay.JokeActivity;
-import com.example.jokesource.JokeProvider;
-
-import junit.framework.TestCase;
 
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
+
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -34,12 +33,17 @@ public class EndpointsAsyncTaskTest{
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void NoEmptyJokeTest(){
-        JokeProvider jokeProvider = new JokeProvider();
-        String joke = jokeProvider.getJoke();
-        Assert.assertTrue(!joke.equals(" "));
-        onView(withId(R.id.joke_button)).perform(click());
-        onView(withId(R.id.display_text)).check(matches(isDisplayed()));
+    public void NoEmptyJokeTest() throws ExecutionException, InterruptedException, TimeoutException {
+
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask();
+        String joke = endpointsAsyncTask.execute(InstrumentationRegistry.getTargetContext()).get(5, TimeUnit.SECONDS);
+        Assert.assertTrue(!joke.equals(""));
+    }
+
+        @Test
+        public void verifyResponse() {
+            onView(withId(R.id.joke_button)).perform(click());
+            onView(withId(R.id.display_text)).check(matches(not(withText(""))));
 
     }
     }
