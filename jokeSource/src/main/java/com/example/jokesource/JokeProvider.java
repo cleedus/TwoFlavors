832b1d485686e8e9958d2841e83d918566ae7b35
Cletus Mukolu <cletus.mukolu@gmail.com>
2018-12-02 08:59:58 -0600

    added one more joke

--- jokeSource/src/main/java/com/example/jokesource/JokeProvider.java
+++ jokeSource/src/main/java/com/example/jokesource/JokeProvider.java
@@ -22,6 +22,7 @@ public class JokeProvider {
                 "A: Because he was always spotted.");
         arrayList.add("Wife: \"In my dream, I saw you in a jewelry store and you bought me a diamond ring.\" \n" +
                 "Husband: \"I had the same dream and I saw your dad paying the bill.\"");
+       arrayList.add("Q: What’s the best time to go to the dentist? A: Tooth hurty.");
 
         arrayList.add("A child asked his father, \"How were people born?\" So his father said, \"Adam and Eve made babies, then their babies became adults and made babies, and so on.\" The child then went to his mother, asked her the same question and she told him, \"We were monkeys then we evolved to become like we are now.\" The child ran back to his father and said, \"You lied to me!\" His father replied, \"No, your mom was talking about her side of the family.");
         return arrayList;
